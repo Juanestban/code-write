d0092d4cba7d1bc920112f2af513164ff726ebea
Juan Esteban <juanestban000@gmail.com>
2021-08-10 17:15:20 -0500

    Creating the simple and similar code-pen

---
+++ src/hooks/useBase64.js
@@ -0,0 +1,23 @@
+export const useBase64 = () => {
+  const transformToBase64 = (stringHTML = '', result) => {
+    const encode = window.btoa(stringHTML)
+    console.log('base64 - transformated')
+    result(encode, stringHTML)
+  }
+
+  const getAllHashed = ({ html, css, js }) => {
+    const $html = window.btoa(html)
+    const $css = window.btoa(css)
+    const $js = window.btoa(js)
+
+    return `${$html}***${$css}***${$js}`
+  }
+
+  const getBase64ToString = (array = []) =>
+    array.map((string, index) => {
+      const toTransform = index === 0 ? string.slice(1) : string
+      return window.atob(toTransform)
+    })
+
+  return { transformToBase64, getAllHashed, getBase64ToString }
+}
